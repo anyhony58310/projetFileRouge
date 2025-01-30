a89b6216d6a77cf50b841579edb4bb3cf8219e61
anyhony58310 <anthony.regala58@gmail.com>
2025-01-30 09:32:48 +0100

    schema products and categories

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("565cd6ebd6f16694")
+/******/ 		__webpack_require__.h = () => ("275f5b261170f225")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "de1fba4ee77f2d17"; }
+/******/ 		__webpack_require__.h = function() { return "5f5dda492bf5f8c0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
